a5deed2baa18dc6f53a2eb55838c48e3cfe3506d
echoeist <fatihahwdw@gmail.com>
2025-06-18 04:18:33 +0800

    Task 1: Created a Responsive Navigation Bar

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css'],
+})
+export class App {
+  title = 'bootstrap-assessment-angular';
+}
