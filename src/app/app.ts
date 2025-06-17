8886aea26f243559a24629be0baef0e55716f1f8
echoeist <fatihahwdw@gmail.com>
2025-06-18 06:18:57 +0800

    chore: Task 5 - Implement SPA logic using Angular with form validation

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,7 +9,6 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
-
 export class App {
   protected title = 'bootstrap-assessment-angular';
 
